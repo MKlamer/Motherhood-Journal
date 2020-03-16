19edcbd50cd7a17347018816b943530bd6a5028d
Elisa Beshero-Bondar <ebbondar@gmail.com>
2020-03-16 17:27:43 -0400

    updated JS and CSS for normalized view with sic reg toggling

--- WebsiteDirectory-gaskelldigitaledition/scripts/gde-script.js
+++ WebsiteDirectory-gaskelldigitaledition/scripts/gde-script.js
@@ -6,14 +6,28 @@
 
 window.onload = init;
 
-function init() {
+function init() /* ebb: Here's where Being is but an Ear! Add your event listeners here and set up what functions you want to fire next. */
+{
     var anchors = document.getElementsByClassName("ref");
+    var prosopRefs = document.getElementsByClassName('pIndex');
+    var sicReg = document.getElementById('REGtoggle');
+    
+    sicReg.addEventListener('click', toggle, false);
+    
     for (var i = 0; i < anchors.length; i++)
     {
         anchors[i].addEventListener('mouseover', show_footnote, false);
         anchors[i].addEventListener('click', show_footnote, false);
         anchors[i].addEventListener('mouseout', hide_footnote, false);
         anchors[i].addEventListener('dblclick', hide_footnote, false);
+        }
+    for (var i = 0; i < prosopRefs.length; i++)
+    {
+        prosopRefs[i].addEventListener('mouseover', show_footnote, false);
+        prosopRefs[i].addEventListener('click', show_footnote, false);
+        prosopRefs[i].addEventListener('mouseout', hide_footnote, false);
+        prosopRefs[i].addEventListener('dblclick', hide_footnote, false);
+        
         }
         }
         
@@ -32,48 +46,19 @@ var fieldset = document.getElementsByTagName('input');
     for (var j = 0; j < fieldset.length; j++) {
         fieldset[j].addEventListener('click', toggle, false);
 
-var personography = Document.getElementById('pIndexNote');
-
-function show_personography() {
-    var personography = this.child/note;
-    console.log('personography = ' + footnote);
-    footnote.style.display = "inline";
 }
-
-function hide_personography() {
-    var footnote = this.child/note;
-    footnote.style.display = "none";
-}
-function toggle () {
-   var id = REGtoggle
-   switch (id) {
-       case "sic": {
-           var edView = document.getElementsByClassName('sic');
-           for (var i = 0; i < sic.length; i++) {
-               sic[i].classList.toggle("on")
-           }
-           }
-           break;
-      case "REG": {
-          var regView = document.getElementsByClassName('reg');
-          for (var i=0; i < reg.length; i++) {
-              reg[i].classList.toggle("on")   
-      }
-      }
-      break;
-    }
-   }
-   
-   var edView = document.getElementsByClassName('sic');
-    console.log('sic =' +sic);
-    sic.style.display = "inline";
-       
-    var regView = document.getElementsByClassName('reg');
-    console.log('reg =' +reg);
-    reg.style.display = "hidden";
-     
+function toggle() {
+   var sics = document.getElementsByClassName("sic");
+            for (var i = 0; i < sics.length; i++) {
+                sics[i].classList.toggle("on")
+            }
+    var regs = document.getElementsByClassName("reg");
+            for (var i = 0; i < regs.length; i++) {
+                regs[i].classList.toggle("on")
+            }     
   
 }
+
  
 
 
