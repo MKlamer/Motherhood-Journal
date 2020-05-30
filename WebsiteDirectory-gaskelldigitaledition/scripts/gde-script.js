3d503b73c872f837db7e3fa9132254fa4bc68f84
MKlamer <klamerme@msu.edu>
2020-05-30 11:26:36 -0400

    Testing responsive size design on index.html

--- WebsiteDirectory-gaskelldigitaledition/scripts/gde-script.js
+++ WebsiteDirectory-gaskelldigitaledition/scripts/gde-script.js
@@ -74,7 +74,15 @@ function toggle() {
                 tocHeight[i].classList.toggle("on")
             }
 }
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("navlink");
+  if (x.className === "navlink") {
+    x.className += " responsive";
+  } else {
+    x.className = "navlink";
+  }
+}
  
 
 
